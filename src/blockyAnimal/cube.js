ee45a8d62aa2ef437a56f6b24f1e73d74744ebd7
Katarina Bjegovic <kbjegovi@ucsc.edu>
2025-02-03 02:29:50 -0800

    shading

--- src/blockyAnimal/cube.js
+++ src/blockyAnimal/cube.js
@@ -12,45 +12,41 @@ class Cube{
         var rgba = this.color;
         //var size = this.size;
         //gl.vertexAttrib3f(a_position, xy[0], xy[1], 0.0);
-        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-        //gl.uniform1f(u_size, size);
+        // gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+        // //gl.uniform1f(u_size, size);
 
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
 
-        gl.uniform4f(u_FragColor, rgba[0]*0.9, rgba[1]*0.9, rgba[2]*0.9 , rgba[3]);
+       
         // other sides of the cube... 
         // Define all six faces (two triangles per face)
-        let vertices = [
-            // Front face
-            0, 0, 0,  1, 1, 0,  1, 0, 0,
-            0, 0, 0,  0, 1, 0,  1, 1, 0,
+        gl.uniform4f(u_FragColor, rgba[0]*0.85, rgba[1]*0.85, rgba[2]*0.85 , rgba[3]);
+// Front face
+        drawTriangle3D(0, 0, 0,  1, 1, 0,  1, 0, 0);
+        drawTriangle3D(0, 0, 0,  0, 1, 0,  1, 1, 0,);
+//back face
+        drawTriangle3D(0, 0, 1,  1, 1, 1,  1, 0, 1);
+        drawTriangle3D(0, 0, 1,  0, 1, 1,  1, 1, 1);
 
-            // Back face
-            0, 0, 1,  1, 1, 1,  1, 0, 1,
-            0, 0, 1,  0, 1, 1,  1, 1, 1,
+//top face
+        gl.uniform4f(u_FragColor, rgba[0]*0.9, rgba[1]*0.9, rgba[2]*0.9 , rgba[3]);
+        
+        drawTriangle3D(0, 1, 0,  1, 1, 1,  0, 1, 1);
+        drawTriangle3D(0, 1, 0,  1, 1, 0,  1, 1, 1);
+//bottom face
+        drawTriangle3D(0, 0, 0,  1, 0, 1,  0, 0, 1);
+        drawTriangle3D(0, 0, 0,  1, 0, 0,  1, 0, 1);
 
-            // Left face
-            0, 0, 0,  0, 1, 1,  0, 1, 0,
-            0, 0, 0,  0, 0, 1,  0, 1, 1,
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+//left and right sides
 
-            // Right face
-            1, 0, 0,  1, 1, 1,  1, 1, 0,
-            1, 0, 0,  1, 0, 1,  1, 1, 1,
+        drawTriangle3D(0, 0, 0,  0, 1, 1,  0, 1, 0);
+        drawTriangle3D(0, 0, 0,  0, 0, 1,  0, 1, 1);
 
-            // Top face
-            0, 1, 0,  1, 1, 1,  0, 1, 1,
-            0, 1, 0,  1, 1, 0,  1, 1, 1,
+        drawTriangle3D(1, 0, 0,  1, 1, 1,  1, 1, 0);
+        drawTriangle3D(1, 0, 0,  1, 0, 1,  1, 1, 1);
 
-            // Bottom face
-            0, 0, 0,  1, 0, 1,  0, 0, 1,
-            0, 0, 0,  1, 0, 0,  1, 0, 1
-        ];
-
-        // Draw each triangle
-        for (let i = 0; i < vertices.length; i += 9) {
-            drawTriangle3D(vertices.slice(i, i + 9));
-        }
     }
 }
 
